0bfc527e0f30ef38efb4a6c92dac0209ed02505f
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-09-14 22:06:16 +0530

    ui completed

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -10,20 +11,23 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      react.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/jsx-no-target-blank': 'off',
+      'react/prop-types': 'off',
     },
   },
 ])
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,26 @@ import { Route , Routes} from "react-router-dom"
 import HomePage from "./pages/Home/HomePage.jsx"
 import LoginPage from "./pages/auth/login/LogInPage.jsx"
 import SignPage from "./pages/auth/signup/signupPage.jsx"
+import NotificationPage from "./pages/notification/NotificationPage.jsx"
+import ProfilePage from "./pages/profile/ProfilePage.jsx"
+import Sidebar from "./components/common/Sidebar.jsx"
+import RightPanel from "./components/common/RightPanal.jsx"
 
 function App() {
  
 
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar/>
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/signup" element={<SignPage/>}/>
         <Route path="/login" element={<LoginPage />}/>
+        <Route path="/notifications" element={<NotificationPage/>}/>
+        <Route path="/profile/:username" element={<ProfilePage/>}/>
 
       </Routes>
+      <RightPanel/>
    
     </div>
   )
